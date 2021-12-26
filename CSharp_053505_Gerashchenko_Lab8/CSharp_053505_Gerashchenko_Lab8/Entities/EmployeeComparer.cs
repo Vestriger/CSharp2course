5261c4e71c9089af00b322db0299ce5b5a020fe5
Vestriger <gerashchenko.p@mail.ru>
2021-12-27 01:20:26 +0300

    lab8

---
+++ CSharp_053505_Gerashchenko_Lab8/CSharp_053505_Gerashchenko_Lab8/Entities/EmployeeComparer.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+
+namespace CSharp_053505_Gerashchenko_Lab8.Entities
+{
+    public class EmployeeComparer : IComparer<Employee>
+    {
+        public int Compare(Employee? x, Employee? y) =>
+            string.Compare(x?.Name, y?.Name, StringComparison.InvariantCulture);
+    }
+}
---
+++ CSharp_053505_Gerashchenko_Lab8/CSharp_053505_Gerashchenko_Lab8/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CSharp_053505_Gerashchenko_Lab8.Entities;
+
+namespace CSharp_053505_Gerashchenko_Lab8
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var session = new FileService();
+
+            var employedByFactory = GetDefaultEmployeesPack();
+            var (firstFileName, secondFileName) =
+                (GetRandomStringFileName(), GetRandomStringFileName());
+
+            Console.WriteLine(firstFileName);
+            Console.WriteLine(secondFileName);
+
+            session.SaveData(employedByFactory, firstFileName);
+
+            File.Move(firstFileName, secondFileName);
+
+            var testReading = session.ReadFile(secondFileName).ToList();
+            testReading.Sort(new EmployeeComparer());
+            testReading.ForEach(Console.WriteLine);
+        }
+
+
+        #region GetDefaultEmployeesPack()
+
+        private static IEnumerable<Employee> GetDefaultEmployeesPack() => new[]
+        {
+            new Employee("John", 300, true),
+            new Employee("Mike", 300, true),
+            new Employee("Walter", 1000, false),
+            new Employee("Mark", 500, false),
+            new Employee("Bob", 650, false)
+        };
+
+        #endregion
+
+        #region GetRandomStringFileName
+
+        private static string GetRandomStringFileName() =>
+            new string(Enumerable
+                .Repeat("abcdefghigklmnopqrstuvwxyz0123456789", new Random().Next(7, 10))
+                .Select(s => s[new Random().Next(s.Length)]).ToArray()) + ".txt";
+
+        #endregion
+    }
+}
